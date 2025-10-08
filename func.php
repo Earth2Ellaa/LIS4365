80ea0a4af46f1d5f441a3b4cd34c0c650cb5ed61
Earth2Ellaa <168940987+Earth2Ellaa@users.noreply.github.com>
2025-10-08 15:48:37 -0400

    Add files via upload

---
+++ func.php
@@ -0,0 +1,65 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Module4</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+<!-- Header -->
+  <header id="header">
+    <nav class="left">
+      <a href="#menu"><span>Menu</span></a>
+    </nav>
+    <a href="index.html" class="logo">Web Design Technologies</a>
+    <nav class="right">
+      <a href="#" class="button alt">Contact</a>
+    </nav>
+  </header>
+
+  <!-- Menu -->
+  <nav id="menu">
+    <ul class="links">
+      <li><a href="index.html">Home</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="assignments.html">Assignments</a></li>
+    </ul>
+    <ul class="actions vertical">
+      <li><a href="#" class="button fit">Contact</a></li>
+    </ul>
+  </nav>
+
+<!-- Main Section -->
+<section id="main" class="wrapper">
+  <div class="inner">
+    <header class="align-center">
+      <h1>Module 4</h1>
+      <p>Introduction to Photoshop</p>
+    </header>
+
+    <!-- Assignment Image -->
+    <span class="image fit">
+      <img src="pic3.jpg" alt="Assignment 1">
+    </span>
+
+    <!-- Assignment Content -->
+    <h2><i>Content</i></h2>
+	<h3> September 21st : Testing Loops and Using PhotoShop</h3>
+	
+<?php
+function writeMsg() {
+  echo "Hello world!";
+}
+
+writeMsg(); // call the function
+?>
+</footer>
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/main.js"></script>
+<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script><script src="assets/js/jquery.scrolly.min.js"></script><script src="assets/js/skel.min.js"></script><script src="assets/js/util.js"></script><script src="assets/js/main.js"></script></body></html>
+</body>
+</html>
